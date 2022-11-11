3542252d3e897e69230e3d563905aea922d62e38
david <leachda12@gmail.com>
2022-11-11 09:54:45 -0500

    built class for user info

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-// fire on page load
+// ----- user info class ----- //
+
+class userInfo {
+    constructor(username, firstName, email, password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+
+
+// ----- fire on page load ----- //
+
 $(document).ready(() => {
     console.log('test');
 });
 
+
+
+$('.sup-submit').click((e) => {
+    
+})
